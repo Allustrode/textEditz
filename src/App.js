cfc40336bbf70c2cc8d8e4e9c8e13d0e274a9e96
Allustrode <79183292+Allustrode@users.noreply.github.com>
2022-12-01 11:06:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Form/>
+    <footer className='footer'>
+      | Developed by Allustrode |
+    </footer>
+    </>
+  );
+}
+
+export default App;
